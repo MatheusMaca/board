a9750e7cfd4e4e5a4d11aaa5e76833ca45cbf207
matheusmaca <macariniking@gmail.com>
2022-04-06 18:01:58 -0300

    Editando items da lista de tarefas

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/client';
 import Link from 'next/link';
 import styles from './styles.module.scss';
-import { FiCalendar, FiClock, FiEdit2, FiPlus, FiTrash } from 'react-icons/fi';
+import { FiCalendar, FiClock, FiEdit2, FiPlus, FiTrash, FiX } from 'react-icons/fi';
 import { SupportButton } from '../../components/SupportButton';
 import firebase from '../../services/firebaseConnection';
 import { format } from 'date-fns';
@@ -29,12 +29,33 @@ type TaskList = {
 export default function Board({ user, data }: BoardProps) {
     const [input, setInput] = useState('');
     const [taskList, setTaskList] = useState<TaskList[]>(JSON.parse(data));
+    const [taskEdit, setTaskEdit] = useState<TaskList | null>(null);
 
     async function handleAddTask(e: FormEvent) {
         e.preventDefault();
 
         if (input === '') {
             alert('Preencha o campo tarefa');
+            return;
+        }
+
+        if(taskEdit){
+            await firebase.firestore().collection('tarefas').doc(taskEdit.id)
+            .update({
+                tarefa: input
+            })
+            .then(()=>{
+                let data = taskList;
+                let taskIndex = taskList.findIndex(item => item.id === taskEdit.id);
+                data[taskIndex].tarefa = input;
+                setTaskList(data);
+                setTaskEdit(null);
+                setInput('');
+            })
+            .catch((err) => {
+                console.log('Erro ao atualizar: ', err);
+            })
+
             return;
         }
 
@@ -77,12 +98,31 @@ export default function Board({ user, data }: BoardProps) {
             });
     }
 
+    function handleEditTask(task: TaskList) {
+        setTaskEdit(task);
+        setInput(task.tarefa);
+        
+    }
+
+    function handleCancelEdit() {
+        setInput('');
+        setTaskEdit(null);
+    }
+
     return (
         <>
             <Head>
                 <title>Minhas tarefas - Board</title>
             </Head>
             <main className={styles.container}>
+                {taskEdit && (
+                    <span className={styles.warnText}>
+                        <button onClick={handleCancelEdit}>
+                            <FiX size={30} color="#FF3636" />
+                        </button>
+                        Você está editando uma tarefa!
+                    </span>
+                )}
                 <form onSubmit={handleAddTask}>
                     <input
                         type="text"
@@ -108,7 +148,7 @@ export default function Board({ user, data }: BoardProps) {
                                         <FiCalendar size={20} color="#FFB800" />
                                         <time>{task.createdFormated}</time>
                                     </div>
-                                    <button>
+                                    <button onClick={() => handleEditTask(task)}>
                                         <FiEdit2 size={20} color="#FFF" />
                                         <span>Editar</span>
                                     </button>
