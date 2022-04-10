873d29efed04575997bf2873c8e7f3ec8676a7e8
matheusmaca <macariniking@gmail.com>
2022-04-10 11:31:18 -0300

    Consumindo dados para popular página detalhes

--- src/pages/board/[Id].tsx
+++ src/pages/board/[Id].tsx
@@ -1,7 +1,64 @@
-export default function Task() {
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/client"
+import firebase from "../../services/firebaseConnection"
+import { format } from "date-fns"
+
+type Task = {
+    id: string;
+    created: string | Date;
+    createdFormated?: string;
+    tarefa: string;
+    userId: string;
+    nome: string;
+}
+
+interface TaskListProps {
+    data: string;
+}
+
+export default function Task({ data }: TaskListProps) {
+    const task = JSON.parse(data) as Task;
+
     return (
         <div>
             <h1>Página detalhes</h1>
         </div>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+    const { id } = params;
+    const session = await getSession({ req });
+
+    if (!session?.id) {
+        return {
+            redirect: {
+                destination: '/board',
+                permanent: false
+            }
+        }
+    }
+
+    const data = await firebase.firestore()
+        .collection('tarefas')
+        .doc(String(id))
+        .get()
+        .then((snapshot) => {
+            const data = {
+                id: snapshot.id,
+                created: snapshot.data().created,
+                createdFormated: format(snapshot.data().created.toDate(), 'dd MMMM yyyy'),
+                tarefa: snapshot.data().tarefa,
+                userId: snapshot.data().userId,
+                nome: snapshot.data().nome
+            }
+
+            return JSON.stringify(data);
+        })
+
+    return {
+        props: {
+            data
+        }
+    }
 }
